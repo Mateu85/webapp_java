1639d77d2ed0d52d605303139f72795a47a53641
vicent.mateu <vicent.mateu@onlean.es>
2022-03-25 18:57:31 +0100

    refactor

---
+++ src/com/svalero/webapp/WebApp.java
@@ -0,0 +1,9 @@
+package com.svalero.webapp;
+
+public class WebApp {
+
+    public static void main(String[] args) {
+        OptionsMenu optionsMenu = new OptionsMenu();
+        optionsMenu.mostrarMenu();
+    }
+}
--- src/com/svalero/webappjava/Aplication.java
+++
@@ -1,9 +0,0 @@
-package com.svalero.webappjava;
-
-public class Aplication {
-
-    public static void main(String[] args) {
-        OptionMenu menu = new OptionMenu();
-        menu.optionMenu();
-    }
-}
